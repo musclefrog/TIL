987ac3c1a4f539c063fa0ed212797cc91b002e89
musclefrog <rlatndls113@gmail.com>
2023-05-20 15:55:27 +0900

    0520 Algorithm

---
+++ Algorithm/CodeUp_기초100제/15_input07.py
@@ -0,0 +1,7 @@
+# 공백을 두고 입력된 정수(integer) 2개를 입력받아 줄을 바꿔 출력해보자.
+
+a, b = input().split() #input().split()을 사용하면, 공백을 기준으로 입력된 값들을 나누어 자른다.
+a = int(a)
+b = int(b)
+print(a)
+print(b)
---
+++ Algorithm/CodeUp_기초100제/16_input08.py
@@ -0,0 +1,6 @@
+# 공백을 두고 문자(character) 2개를 입력받아 순서를 바꿔 출력해보자. 
+
+c1, c2 = input().split()
+c1 = str(c1)
+c2 = str(c2)
+print(c2, c1)
