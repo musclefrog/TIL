c4f4e251b256e91c28e07de8e5cb12604152aff0
musclefrog <rlatndls113@gmail.com>
2023-07-08 22:02:36 +0900

    0708 SwiftUI WeatherApp

---
+++ SwiftUI/WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by 김수인 on 2023/07/08.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
