6d258c600a3dcb4e94baddf0f34ac7580bd40cf6
musclefrog <rlatndls113@gmail.com>
2023-07-09 15:54:29 +0900

    0709 Algorithm

--- SwiftUI/WeatherApp/WeatherApp/ContentView.swift
+++ SwiftUI/WeatherApp/WeatherApp/ContentView.swift
@@ -6,14 +6,178 @@
 //
 
 import SwiftUI
+import CoreLocation
+import WeatherKit
+import Charts
+
+// 위치 제공 클래스
+class LocationManager: NSObject, ObservableObject {
+    
+    @Published var currentLocation: CLLocation?
+    private let locationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        locationManager.delegate = self
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        guard let location = locations.last, currentLocation == nil else { return }
+        
+        DispatchQueue.main.async {
+            self.currentLocation = location
+        }
+    }
+}
+
+extension Date {
+    func formatAsAbbreviatedDay() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEE"
+        return formatter.string(from: self)
+    }
+    
+    func formatAsAbbreviatedTime() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "ha"
+        return formatter.string(from: self)
+    }
+}
+
+// Hourly forecast view
+struct HourlyForecastView: View {
+    
+    let hourWeatherList: [HourWeather]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("HOURLY FORECAST")
+                .font(.caption)
+                .opacity(0.5)
+            ScrollView(.horizontal) {
+                HStack {
+                    ForEach(hourWeatherList, id: \.date) { hourWeatherItem in
+                        VStack(spacing: 20) {
+                            Text(hourWeatherItem.date.formatAsAbbreviatedTime())
+                            Image(systemName: "\(hourWeatherItem.symbolName).fill")
+                                .foregroundColor(.yellow)
+                            Text(hourWeatherItem.temperature.formatted())
+                                .fontWeight(.medium)
+                        }.padding()
+                    }
+                }
+            }
+        }.padding().background {
+            Color.blue
+        }.clipShape(RoundedRectangle(cornerRadius: 10.0, style: .continuous))
+            .foregroundColor(.white)
+    }
+}
+
+// 10 days forecast view
+struct TenDayForecastView: View {
+    
+    let dayWeatherList: [DayWeather]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("10-DAY FORECAST")
+                .font(.caption)
+                .opacity(0.5)
+            
+            List(dayWeatherList, id: \.date) { dailyWeather in
+                HStack {
+                    Text(dailyWeather.date.formatAsAbbreviatedDay())
+                        .frame(maxWidth: 50, alignment: .leading)
+                    
+                    Image(systemName: "\(dailyWeather.symbolName)")
+                        .foregroundColor(.yellow)
+                    
+                    Text(dailyWeather.lowTemperature.formatted())
+                        .frame(maxWidth: .infinity)
+                    
+                    Text(dailyWeather.highTemperature.formatted())
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                }.listRowBackground(Color.blue)
+            }.listStyle(.plain)
+        }
+        .frame(height: 300).padding()
+        .background(content: {
+            Color.blue
+        })
+        .clipShape(RoundedRectangle(cornerRadius: 10.0, style: .continuous))
+        .foregroundColor(.white)
+    }
+}
+
+// Hourly forecast chart
+struct HourlyForecastChartView: View {
+    
+    let hourlyWeatherData: [HourWeather]
+    
+    var body: some View {
+        Chart {
+            ForEach(hourlyWeatherData.prefix(10), id: \.date) { hourlyWeather in
+                LineMark(x: .value("Hour", hourlyWeather.date.formatAsAbbreviatedTime()), y: .value("Temperature", hourlyWeather.temperature.converted(to: .fahrenheit).value))
+            }
+        }
+    }
+}
 
 struct ContentView: View {
+    
+    let weatherService = WeatherService.shared
+    // LocationManager 사용
+    @StateObject private var locationManager = LocationManager()
+    @State private var weather: Weather?
+    
+    var hourlyWeathereData: [HourWeather] {
+        if let weather {
+            return Array(weather.hourlyForecast.filter { hourlyWeather in
+                return hourlyWeather.date.timeIntervalSince(Date()) >= 0
+            }.prefix(24))
+        } else {
+            return []
+        }
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            // 날씨를 화면에 보여주기
+            if let weather {
+                VStack {
+                    Text("Pohang")
+                        .font(.largeTitle)
+                    Text("\(weather.currentWeather.temperature.formatted())")
+                }
+                
+                HourlyForecastView(hourWeatherList: weather.hourlyForecast.forecast)
+               // Spacer()
+                
+                HourlyForecastChartView(hourlyWeatherData: hourlyWeathereData)
+                
+                TenDayForecastView(dayWeatherList: weather.dailyForecast.forecast)
+            }
+        }
+        .padding()
+        .task(id: locationManager.currentLocation) {
+            // location에 따른 날씨를 fetch 하기
+            // 앱이 로딩 되자마자 날씨 정보 받기
+            do {
+                // if let location = locationManager.currentLocation {
+                let location = CLLocation(latitude: 36.019016, longitude: 129.343475)
+                self.weather = try await weatherService.weather(for: location)
+                // }
+            } catch {
+                print(error)
+            }
         }
         .padding()
     }
